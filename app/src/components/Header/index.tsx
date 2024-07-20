6402f4641d57b6641c98041b4c8e99f488b02e98
Alex Sander <alexsander01@hotmail.com.br>
2024-07-20 10:28:08 -0300

    refactor: changed the name

--- app/src/components/Header/index.tsx
+++ app/src/components/Header/index.tsx
@@ -15,8 +15,9 @@ export function Header({setSelectedTable, onCancelOrder}: HeaderProps){
                 <>
                     <Text size={14} opacity={0.9}>Bem-vindo(a) ao</Text>
                         <Text size={24} weight='700'>
-                         WAITER
-                        <Text size={24}>APP</Text>
+                         PIZZA
+
+                        <Text size={24}>delicious</Text>
                     </Text>
                 </>
             )}
