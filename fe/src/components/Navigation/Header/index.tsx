1d0289e597881303671cc28b32d848dabd6192ec
Alex Sander <alexsander01@hotmail.com.br>
2024-08-03 10:54:03 -0300

    feat: rendering signIn and signUp in the header

--- fe/src/components/Navigation/Header/index.tsx
+++ fe/src/components/Navigation/Header/index.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { NavBar } from "../NavBar/navBar";
-import { CartContainer, Container, Content, CartBadge } from "./styled";
+import { CartContainer, Container, Content, CartBadge, Login, Register, SignUpContainer, ContentConateiner } from "./styled";
 import logoPizza from '../../../assets/images/logoPizza.png';
 import { IoMdCart } from "react-icons/io";
+import { FaUserAlt } from "react-icons/fa";
+import { SignIn } from "../../Login/signIn";
+import { SignUp } from "../../Login/signUp";
 
 interface HeaderProps {
     onOpenCartModal: () => void;
@@ -9,15 +13,47 @@ interface HeaderProps {
 }
 
 export function Header({ onOpenCartModal, totalQuantity }: HeaderProps) {
+    const [isSignInVisible, setIsSignInVisible] = useState(false);
+    const [isSignUpVisible, setIsSignUpVisible] = useState(false);
+
+    function handleOpenSignInModal() {
+        setIsSignInVisible(true);
+        setIsSignUpVisible(false);
+    }
+
+    function handleOpenSignUpModal() {
+        setIsSignUpVisible(true);
+        setIsSignInVisible(false);
+    }
+
+    function handleCloseModal() {
+        setIsSignInVisible(false);
+        setIsSignUpVisible(false);
+    }
+
     return (
         <Container>
             <Content>
                 <img src={logoPizza} alt="WaiterApp" />
                 <NavBar />
+                <Login>
+                    <FaUserAlt size={20} color="#ffff" />
+                    <ContentConateiner>
+                        <SignUpContainer>
+                            <span>Faça seu <b onClick={handleOpenSignInModal}>Login</b> ou </span>
+                        </SignUpContainer>
+                        <Register>
+                            <span>crie seu <b onClick={handleOpenSignUpModal}>Cadastro</b></span>
+                        </Register>
+                    </ContentConateiner>
+                </Login>
                 <CartContainer onClick={onOpenCartModal}>
                     <IoMdCart size={30} color="#fff" />
                     {totalQuantity > 0 && <CartBadge>{totalQuantity}</CartBadge>}
                 </CartContainer>
+
+                {isSignInVisible && <SignIn onRequestClose={handleCloseModal} />}
+                {isSignUpVisible && <SignUp onRequestClose={handleCloseModal} />}
             </Content>
         </Container>
     );
