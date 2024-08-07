323b85fb449dd6ff8c74686d400d2bf4a3b95c67
Alex Sander <alexsander01@hotmail.com.br>
2024-08-07 00:19:03 -0300

    chore: adjusted for the ingredients to go to the cart with the condition that it does not replicate with the quantities

--- fe/src/types/CartItem.ts
+++ fe/src/types/CartItem.ts
@@ -2,7 +2,8 @@ import { Product } from "../Type/Product";
 
 
 
-export interface CartItem{
+export interface CartItem {
     product: Product;
     quantity: number;
+    selectedIngredients: string[];  // Adicionando ingredientes selecionados
 }
